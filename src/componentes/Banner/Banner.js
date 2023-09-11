49243747bb7dcc7790be75a52ef8422ce56c5013
renata-aline <renatalinec@gmail.com>
2023-09-11 20:41:39 -0300

    limpando o código e adicionando header da página

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página organo" />
+        </header>
+    )
+
+}
+
+export default Banner
