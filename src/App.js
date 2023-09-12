3523b8ad7630202e6d81278dcc7546a7d18a8c35
renata-aline <renatalinec@gmail.com>
2023-09-12 16:47:40 -0300

    campo de texto e parâmetros de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
 
     </div>
   );
