640e52b0cf3b3a62e7bbd48ff5ad1632b297f481
renata-aline <renatalinec@gmail.com>
2023-09-18 16:01:07 -0300

    cadastrando colaboradores

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,7 +5,7 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-End',
@@ -23,7 +23,13 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('form foi submetido =>', nome, cargo, imagem,time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        
     }
 
     return (
